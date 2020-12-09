5e24f5ef1ef9043ab69ffa93c4ac14a52ad52fba
JaredScar <thewolfbadger@gmail.com>
2020-12-08 22:38:37 -0500

    + Figured out how to get Hurricane Tracking Data + Implremented Hurricane data into map with markers + Added more to About Us page (need to credit everyone)
    TODO:
    [x] Figure out how to get Hurricane Tracking Data
    [x] Implement Hurricane data into maps with markers
    [ ] Implement hovers for markers including more information about said Hurricane
    [ ] Implement overlay over map if no hurricanes found
    [ ] Finish Get Prepared page
    [ ] Finish Home page
    [ ] Finish About Us page

--- js/StaticMap.js
+++ js/StaticMap.js
@@ -19,10 +19,54 @@ var map = new ol.Map({
     interactions: [],
 });
 
-var hurricaneData = {};
 function setupHurricaneData() {
-    // TODO
+    var url = "https://www.nhc.noaa.gov/productexamples/NHC_JSON_Sample.json";
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"; // Free proxy
+    var data = null;
+    fetch(proxyurl + url) // We use a proxy so we can avoid Access-Control-Allow-Origin problem
+        .then(response => response.text())
+        .then(function(contents) {
+            data = contents;
+            console.log(data);
+            var obj = JSON.parse(data);
+            var count = 1;
+            for (var i = 0; i < Object.keys(obj.activeStorms).length; i++) {
+                var storm = obj.activeStorms[i];
+                var name = storm.name;
+                /**
+                 * SOURCE: https://nws.weather.gov/products/PDD/PDD_ExpTropicalCycloneJSONFile_2019.pdf
+                 *
+                 * Classifications:
+                     * TD = Tropical Depression
+                     * STD = Subtropical Depression
+                     * TS = Tropical Storm
+                     * HU = Hurricane
+                     * STS = Subtropical Storm
+                     * PTC = Post-tropical Cyclone / Remnants
+                     * TY = Typhoon [ NOT USED ]
+                     * PC = Potential Tropical Cyclone
+                 */
+                var classification = storm.classification;
+                var longitude = storm.longitudeNumeric;
+                var latitude = storm.latitudeNumeric;
+                var windMPH = storm.intensity;
+                var pressure = storm.pressure;
+                $('#marker-' + count.toString()).attr('title', "Hurricane " + name);
+
+                var pos = ol.proj.fromLonLat([longitude, latitude]);
+                var marker = new ol.Overlay({
+                    position: pos,
+                    positioning: 'center-center',
+                    element: document.getElementById('marker-' + count.toString()),
+                    //stopEvent: false
+                });
+                map.addOverlay(marker);
+                count++;
+            }
+        })
+        .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 }
+setupHurricaneData();
 
 /** /
  var pos = ol.proj.fromLonLat([23.3725, 35.208889]);
